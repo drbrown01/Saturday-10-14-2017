42294503500292d5f086a59ed3f0c4603cadc2fd
Dan <drbrown01@yahoo.com>
2017-10-14 10:55:33 -0500

    server created node.js javascript

--- demo/server.js
+++ demo/server.js
@@ -1,10 +1,14 @@
 var http = require("http");
 
-var port = 80; //port is umprivelidge, doesnt' require root access to run
+var port = 8080; //port is umprivelidge, doesnt' require root access to run
 
 function handleRequest(request, response) {
-  response,end("It works"+ request.url);
-}
+  response.end("It works"+ request.url);
+};
 
 // server side stuff goes here.
 var server = http.createServer(handleRequest);
+
+server.listen(port, function() {
+  console.log("we are listening" + port);
+});
